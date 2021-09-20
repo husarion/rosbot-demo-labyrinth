879df3c80229e241ddd174773cd5a5c8b63232b4
Dawid Kmak <kmakd197@gmail.com>
2021-09-20 14:59:12 +0200

    organize code

--- src/maze_cam/launch/maze_cam.launch.py
+++ src/maze_cam/launch/maze_cam.launch.py
@@ -1,7 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-
 import os
 
 def generate_launch_description():
@@ -13,8 +12,6 @@ def generate_launch_description():
             package='v4l2_camera',
             executable='v4l2_camera_node',
             name='v4l2_camera_node',
-            # parameters=[
-            #     {'video_device': video_device}],
             parameters=[
                 maze_cam + '/config/v4l2_camera_params.yaml',
                 {'video_device': video_device}
