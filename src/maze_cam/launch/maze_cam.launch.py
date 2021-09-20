b73d9451ed969cc44bacd7857540ff8a343f5c1a
Dawid Kmak <kmakd197@gmail.com>
2021-09-20 14:46:57 +0200

    add VIDEO_DEVICE env variable

--- src/maze_cam/launch/maze_cam.launch.py
+++ src/maze_cam/launch/maze_cam.launch.py
@@ -2,17 +2,22 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
+import os
+
 def generate_launch_description():
     maze_cam = get_package_share_directory('maze_cam')
-
+    video_device = os.environ.get('VIDEO_DEVICE')
 
     return LaunchDescription([
         Node(
             package='v4l2_camera',
             executable='v4l2_camera_node',
             name='v4l2_camera_node',
+            # parameters=[
+            #     {'video_device': video_device}],
             parameters=[
-                maze_cam + '/config/v4l2_camera_params.yaml'
+                maze_cam + '/config/v4l2_camera_params.yaml',
+                {'video_device': video_device}
             ]
         ),
 
